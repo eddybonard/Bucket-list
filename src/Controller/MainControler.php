fa91572e91c75ff1ed2ddbe94697b090c53c8a75
eddybONRD <eddyikzh@gmail.com>
2021-07-02 09:28:14 +0200

    creation de entity Wish

--- src/Controller/MainControler.php
+++ src/Controller/MainControler.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainControler extends AbstractController
@@ -13,9 +14,18 @@ class MainControler extends AbstractController
      * @Route("/", name="main_home")
      */
 
-    public  function home()
+    public  function home(): Response
     {
         return $this->render("main/acceuil.html.twig");
+    }
+
+    /**
+     * @Route("/aboutUs", name="main_aboutUs")
+     */
+
+    public  function aboutUs(): Response
+    {
+        return $this->render("main/aboutUs.html.twig");
     }
 
 
