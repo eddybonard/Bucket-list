bd5903495b4a8f8347902317e7fa1050a2cc5211
eddybONRD <eddyikzh@gmail.com>
2021-07-07 15:09:08 +0200

    creation des authentifications

--- src/Controller/MainControler.php
+++ src/Controller/MainControler.php
@@ -21,7 +21,10 @@ class MainControler extends AbstractController
 
     public  function home(): Response
     {
-        return $this->render("main/acceuil.html.twig");
+        $url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.1839486564413!2d-73.98773128501851!3d40.757978742739105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855c6480299%3A0x55194ec5a1ae072e!2sTimes%20Square!5e0!3m2!1sfr!2sfr!4v1625582381617!5m2!1sfr!2sfr";
+        return $this->render("main/acceuil.html.twig", [
+            "url" => $url
+        ]);
     }
 
     /**
