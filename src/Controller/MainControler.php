94d410351e00efbe7ec61c39bb1164309576a64b
eddybONRD <eddyikzh@gmail.com>
2021-07-12 14:41:52 +0200

    modification upload image

--- src/Controller/MainControler.php
+++ src/Controller/MainControler.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 
 
 use App\Controller\Services\Censurator\Censurator;
+use App\Controller\Services\uploadImage\UploadImage;
 use App\Entity\Wish;
 use App\Form\WishType;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class MainControler extends AbstractController
 {
@@ -37,7 +40,10 @@ class MainControler extends AbstractController
     /**
      * @Route("/formulaire", name="main_formulaire")
      */
-    public  function  ajouterWish(Censurator $censurator,Request $request, EntityManagerInterface $entityManager): Response
+    public  function  ajouterWish(Censurator $censurator,
+                                  Request $request,
+                                  EntityManagerInterface $entityManager,
+                                  SluggerInterface $slugger): Response
     {
 
         $wish = new Wish();
@@ -49,14 +55,30 @@ class MainControler extends AbstractController
 
         if ($wishForm->isSubmitted() && $wishForm->isValid() )
         {
-
+            $imageFile = $wishForm->get('image')->getData();
+            if ($imageFile)
+            {
+             /*   $imageFileName = $uploadImage->upload($imageFile);
+                $wish->setImage($imageFileName);*/
+                $originalImageName = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeImageName =$slugger->slug($originalImageName);
+                $imageName = $safeImageName.'-'.uniqid().'.'.$imageFile->guessExtension();
+                try {
+                    $imageFile->move(
+                        $this->getParameter('brochures_directory'),
+                    $imageName);
+            }catch (FileException$e)
+                {
+                    return $e->getTrace();
+                }
+                $wish->setImage($imageName);
+            }
             $entityManager->persist($wish);
             $entityManager->flush();
 
             $this->addFlash('success','Votre Wish a été ajoutée ! ');
             return $this->redirectToRoute('wish_details', ['id' => $wish->getId()]);
         }
-
         return $this->render("main/ajouterWish.html.twig", [
             "wishForm" => $wishForm->createView()
         ]);
